808c32138152fe71eaac089c9a36b74169e1e62a
Piotr Rajmanski <prajmanski10@gmail.com>
2022-05-24 15:07:36 +0200

    update index.js

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const fetch = require('node-fetch');
 const cityName = process.argv[2];
 
 const processWeatherData = data => {
-     data.find(stationData => {
-        if (stationData.stacja === cityName) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    const foundData = data.find(stationData => stationData.stacja === cityName);
+    
+    if (!foundData) {
+        console.log('Takiego miasta nie ma');
+        return;
+    } 
+
+    const {
+        cisnienie: pressure,
+        wilgotnosc_wzgledna: humidity,
+        temperatura: temperature,
+    } = foundData;
+
+    const weatherInfo = `In ${cityName} there is ${temperature} degrees, ${humidity}% of humidity and ${pressure}hPa.`;
+    console.log(weatherInfo);
 };
 
 fetch('https://danepubliczne.imgw.pl/api/data/synop')
