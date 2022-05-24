6978806cb82923100c464de644fbf362899bf1b0
Piotr Rajmanski <prajmanski10@gmail.com>
2022-05-24 15:18:37 +0200

    index.js modify

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const { appendFile } = require('fs');
 const fetch = require('node-fetch');
+const { writeFile } = require('fs').promises;
+const { normalize, resolve } = require('path');
 
 const cityName = process.argv[2];
 
-const processWeatherData = data => {
+function safeJoin(base, target) {
+    const targetPath = '.' + normalize('/' + target)
+    return resolve(base, targetPath);
+}
+
+const getDataFileName = city =>  safeJoin(`./data`, `${city}.txt`);
+
+const processWeatherData = async  data => {
     const foundData = data.find(stationData => stationData.stacja === cityName);
     
     if (!foundData) {
@@ -18,7 +28,13 @@ const processWeatherData = data => {
 
     const weatherInfo = `In ${cityName} there is ${temperature} degrees, ${humidity}% of humidity and ${pressure}hPa.`;
     console.log(weatherInfo);
+
+    const dateString = new Date().toLocaleDateString;
+
+    await appendFile(getDataFileName(cityName), `\n${weatherInfo}\n`)
 };
+
+
 
 fetch('https://danepubliczne.imgw.pl/api/data/synop')
 .then(r => r.json())
